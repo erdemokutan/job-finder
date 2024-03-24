cac0fbdca8cee39542f11136a0e07af063275e14
Erdem Okutan <erdemokutan@hotmail.com>
2024-03-25 01:21:20 +0300

    Data Serializer added & GET Method worked on Postman!

--- backend/job/views.py
+++ backend/job/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from .serializer import JobSerializer
+from .models import Job
 # Create your views here.
+
+@api_view(['GET'])
+def getAllJobs(request):
+
+    jobs=Job.objects.all()
+
+    serializer=JobSerializer(jobs,many=True)
+    return Response(serializer.data)
+

35bbf6d69541f51c0eb44f67e967519641c9f783
Erdem Okutan <erdemokutan@hotmail.com>
2024-03-27 00:10:15 +0300

    title & address based search functionalities added

--- backend/job/filters.py
+++ backend/job/filters.py
@@ -4,6 +4,8 @@ from .models import Job
 
 class JobsFilter(filters.FilterSet):
 
+    keyword=filters.CharFilter(field_name='title',lookup_expr='icontains')
+    location=filters.CharFilter(field_name='address',lookup_expr='icontains')
     min_salary=filters.NumberFilter(field_name='salary' or 0,lookup_expr='gte')#gte= greater than equal
     max_salary=filters.NumberFilter(field_name='salary' or 9999999999,lookup_expr='lte')#lte= less than equal
     
@@ -12,4 +14,9 @@ class JobsFilter(filters.FilterSet):
 
     class Meta:
         model=Job
-        fields=('education','jobType','experience','min_salary','max_salary',)
+        fields=('keyword','location','education','jobType','experience','min_salary','max_salary',)
+
+        
+
+
+        

acd63094421f39f6330550d284749a1ea042b278
Erdem Okutan <erdemokutan@hotmail.com>
2024-03-28 02:02:31 +0300

    signup & user serializer added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'django_filters',
 
-    'job.apps.JobConfig'
+    'job.apps.JobConfig',
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,19 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK={
+
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    )
+}
+
+SIMPLE_JWT={
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken')
+}
 
 
 # Internationalization

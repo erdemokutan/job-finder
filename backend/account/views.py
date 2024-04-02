eefec273afb37bf034bf48a4dbca5cc321fd9093
Erdem Okutan <erdemokutan@hotmail.com>
2024-04-03 00:19:29 +0300

    Applied Candidates Class Created

--- backend/account/views.py
+++ backend/account/views.py
@@ -3,10 +3,10 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 
-from .validators import validate_file_extension
-
 from django.contrib.auth.hashers import make_password
-from .serializers import SignUpSerializer,UserSerializer
+
+from .validators import validate_file_extension
+from .serializers import SignUpSerializer, UserSerializer
 
 from rest_framework.permissions import IsAuthenticated
 
@@ -16,27 +16,27 @@ from django.contrib.auth.models import User
 
 @api_view(['POST'])
 def register(request):
-    data=request.data
+    data = request.data
 
-    user=SignUpSerializer(data=data)
+    user = SignUpSerializer(data=data)
 
     if user.is_valid():
         if not User.objects.filter(username=data['email']).exists():
-            user=User.objects.create(
-                first_name=data['first_name'],
-                last_name=data['last_name'],
-                username=data['email'],
-                email=data['email'],
-                password=make_password(data['password'])
-            )
+           user = User.objects.create(
+               first_name = data['first_name'],
+               last_name = data['last_name'],
+               username = data['email'],
+               email = data['email'],
+               password = make_password(data['password'])
+           ) 
 
-            return Response ({
-                'message':'User Registered Successfully!'},
-                status=status.HTTP_200_Ok
+           return Response({
+                'message': 'User Registered Successfully.'},
+                status=status.HTTP_200_OK
             )
         else:
-            return Response ({
-                'error':'User Already Exists!'},
+            return Response({
+                'error': 'User Already Exists.'},
                 status=status.HTTP_400_BAD_REQUEST
             )
 
@@ -48,7 +48,7 @@ def register(request):
 @permission_classes([IsAuthenticated])
 def currentUser(request):
 
-    user=UserSerializer(request.user)
+    user = UserSerializer(request.user)
 
     return Response(user.data)
 
@@ -56,46 +56,41 @@ def currentUser(request):
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
 def updateUser(request):
-    user=request.user
+    user = request.user
 
-    
+    data = request.data    
 
-    data=request.data
-
-    user.first_name=data['first_name']
-    user.last_name=data['last_name']
-    user.username=data['email']
-    user.email=data['email']
+    user.first_name = data['first_name']
+    user.last_name = data['last_name']
+    user.username = data['email']
+    user.email = data['email']
 
     if data['password'] != '':
         user.password = make_password(data['password'])
 
     user.save()
 
-    serializer=UserSerializer(user, many=False)
+    serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
-
 
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
 def uploadResume(request):
 
-    
-    user=request.user
-    resume=request.FILES['resume']
-    
+    user = request.user
+    resume = request.FILES['resume']
 
     if resume == '':
-        return Response({'error':'Please Upload Your Resume!'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    isValidFile=validate_file_extension(resume.name)
+        return Response({ 'error': 'Please upload your resume.' }, status=status.HTTP_400_BAD_REQUEST)
+
+    isValidFile = validate_file_extension(resume.name)
 
     if not isValidFile:
-        return Response({'error':'Only PDF Files are Valid!'}, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response({ 'error': 'Please upload only pdf file.' }, status=status.HTTP_400_BAD_REQUEST)
 
-    user.userprofile.resume=resume
+    serializer = UserSerializer(user, many=False)
+
+    user.userprofile.resume = resume
     user.userprofile.save()
 
-    serializer = UserSerializer(user,many=False)
     return Response(serializer.data)

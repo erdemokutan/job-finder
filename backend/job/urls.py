047583975b22ea10bda55727253f8ce5ea619148
Erdem Okutan <erdemokutan@hotmail.com>
2024-04-03 02:43:04 +0300

    Get Current User Applied Job Endpoint Added

--- backend/job/urls.py
+++ backend/job/urls.py
@@ -12,4 +12,5 @@ urlpatterns=[
     path('me/jobs/applied/',views.getCurrentUserAppliedJobs,name='current_user_applied_jobs'),
     path('me/jobs/',views.getCurrentUserJobs,name='current_user_jobs'),
     path('jobs/<str:pk>/check/',views.isApplied,name='is_applied_to_job'),
-] 
+     path('job/<str:pk>/candidates/',views.getCandidatesApplied,name='get_candidates_applied'),
+]  

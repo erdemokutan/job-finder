a84e4f89e9fda3b4465c29f461121cd10755634f
Erdem Okutan <erdemokutan@hotmail.com>
2024-04-04 20:43:27 +0300

    Dockerfile & docker-compose.yml changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -174,5 +174,5 @@ VIRTUAL_ENV_BASE = os.environ.get('VIRTUAL_ENV')
 
 print(VIRTUAL_ENV_BASE)
 
-GDAL_LIBRARY_PATH =VIRTUAL_ENV_BASE + '/Lib/site-packages/osgeo/gdal304.dll'
-GEOS_LIBRARY_PATH=VIRTUAL_ENV_BASE + '/Lib/site-packages/osgeo/geos_c.dll'
+#GDAL_LIBRARY_PATH =VIRTUAL_ENV_BASE + '/Lib/site-packages/osgeo/gdal304.dll'
+#GEOS_LIBRARY_PATH=VIRTUAL_ENV_BASE + '/Lib/site-packages/osgeo/geos_c.dll'
